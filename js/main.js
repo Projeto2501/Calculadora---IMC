e376271af7ba5d61d8a7d49eeecae0cf553646cb
Pedro Campos <phcampos138@gmail.com>
2024-03-22 17:30:58 +0000

    Edições finais
    #Correções finais.
    #Mudança no estilo da página.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,35 @@
-const form = getElementById(form);
-form.addEventlistener("submit", function (event){
+const form = document.getElementById('form');
+
+form.addEventListener("submit", function(event) {
     event.preventDefault();
 
-    const weight = docuent.getElementById("weight").value;
+    const weight = document.getElementById("weight").value;
     const height = document.getElementById("height").value;
 
-    const bmi = (weight / (height*height)).toFixed(2);
-    const value = document.getElementById ("value");
+    const bmi = (weight / (height * height)).toFixed(2);
 
+    const value = document.getElementById("value");
     let description = "";
 
     value.classList.add("attention");
-    dpcument.getElementById ("infos").classList.remove("hidden");
+    document.getElementById("infos").classList.remove("hidden");
 
-    if(bmi < 18.5){
-        description = "Cuidado! Você está abaixo do peso."
-    } else if (bmi >- 18.5 && bmi <=25){
+    if (bmi < 18.5) {
+        description = "Cuidado! Você está abaixo do peso.";
+    } else if (bmi >= 18.5 && bmi <= 25) {
         description = "Você está no peso ideal.";
         value.classList.remove("attention");
         value.classList.add("normal");
-    }
-    else if (bmi > 30 && bmi <= 35) {
+    } else if (bmi > 25 && bmi <= 30) {
         description = "Cuidado! Você está com sobrepeso.";
-    }
-    else if (bmi >30 && bmi <= 35){
+    } else if (bmi > 30 && bmi <= 35) {
         description = "Cuidado! Você está com obesidade moderada.";
-    }
-    else if  ( bmi > 35 && bmi <=40){
+    } else if (bmi > 35 && bmi <= 40) {
         description = "Cuidado! Você está com obesidade severa.";
-    }
-    else{
-        description = "Cuidado! Você está com obesidade morbida.";
+    } else {
+        description = "Cuidado! Você está com obesidade mórbida.";
     }
 
-    value.textContent = bmi.replace(".",",");
-    document.getElementById ("description").textContent = description;
+    value.textContent = bmi;
+    document.getElementById("description").textContent = description;
 });
